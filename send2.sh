557737114f0d0d37d5d899dd026823bebc1f4431
doktorandahl <david.randahl@pcr.uu.se>
2024-06-24 19:14:11 +0200

    added send2

--- send2.sh
+++ send2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
 #SBATCH -A naiss2023-5-551
-#SBATCH -J normtest1
+#SBATCH -J normtestbw
 #SBATCH -p main
 #SBATCH -t 24:00:00
 #SBATCH -N 1
-#SBATCH -o normaltest1.out
+#SBATCH -o normaltest1bw.out
 Rscript normal_test_mv_dardel_bw.R
