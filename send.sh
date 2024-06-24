7d8d83d3931406c92c999cf0af4b26c18a530cdd
doktorandahl <david.randahl@pcr.uu.se>
2024-06-24 14:51:08 +0200

    updated send.sh

--- send.sh
+++ send.sh
@@ -2,7 +2,7 @@
 #SBATCH -A naiss2023-5-551
 #SBATCH -J normtest1
 #SBATCH -p main
-#SBATCH -t 00:10:00
+#SBATCH -t 24:00:00
 #SBATCH -N 1
 #SBATCH -o normaltest1.out
 Rscript normal_test_mv_dardel.R
